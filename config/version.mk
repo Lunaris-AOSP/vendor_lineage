cd8c416aa9f2f60c30270f0121f9052faa3f124d
Ghosuto <clash.raja10@gmail.com>
2024-12-31 15:26:30 +0000

    vendor: Build gapps

--- config/version.mk
+++ config/version.mk
@@ -1,11 +1,17 @@
 PRODUCT_VERSION_MAJOR = 15
 PRODUCT_VERSION_MINOR = 0
 
+ifeq ($(WITH_GMS), true)
+    LUNARIS_EDITION ?= PIXEL 
+else
+    LUNARIS_EDITION ?= VANILLA
+endif
+
 # Increase CR Version with each major release.
 LUNARIS_VERSION := 2.4
 
 # Internal version
-LINEAGE_VERSION := Lunaris-OS-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(shell date +%Y%m%d)-$(LINEAGE_BUILD)-v$(LUNARIS_VERSION)
+LINEAGE_VERSION := Lunaris-OS-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(shell date +%Y%m%d)-$(LINEAGE_BUILD)-v$(LUNARIS_VERSION)-$(LUNARIS_EDITION)
 
 # Display version
 LINEAGE_DISPLAY_VERSION := v$(LUNARIS_VERSION)-$(shell date +%Y%m%d)
