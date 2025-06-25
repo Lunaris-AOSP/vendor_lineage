a23a41df9c47dc8ff22757e44ca1715848cbe6aa
Ghosuto <clash.raja10@gmail.com>
2025-06-25 12:57:41 +0000

    lineage: Initial rebrand
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: Lup Gabriel <gwolf2u.website@gmail.com>
    Co-authored-by: Hemant Sachdeva <hemant.evolver@gmail.com>
    Change-Id: I35e1b706cf3319212d4f723dd95fcb3126dd85fb
    Signed-off-by: AnierinB <anierin@evolution-x.org>
    
    Change-Id: Ifb02477e4951f207aadc6cb5b0f651f71095022f

--- config/version.mk
+++ config/version.mk
@@ -1,45 +1,19 @@
-PRODUCT_VERSION_MAJOR = 23
+PRODUCT_VERSION_MAJOR = 16
 PRODUCT_VERSION_MINOR = 0
 
-ifeq ($(LINEAGE_VERSION_APPEND_TIME_OF_DAY),true)
-    LINEAGE_BUILD_DATE := $(shell date -u +%Y%m%d_%H%M%S)
-else
-    LINEAGE_BUILD_DATE := $(shell date -u +%Y%m%d)
-endif
-
-# Set LINEAGE_BUILDTYPE from the env RELEASE_TYPE, for jenkins compat
-
-ifndef LINEAGE_BUILDTYPE
-    ifdef RELEASE_TYPE
-        # Starting with "LINEAGE_" is optional
-        RELEASE_TYPE := $(shell echo $(RELEASE_TYPE) | sed -e 's|^LINEAGE_||g')
-        LINEAGE_BUILDTYPE := $(RELEASE_TYPE)
-    endif
-endif
-
-# Filter out random types, so it'll reset to UNOFFICIAL
-ifeq ($(filter RELEASE NIGHTLY SNAPSHOT EXPERIMENTAL,$(LINEAGE_BUILDTYPE)),)
-    LINEAGE_BUILDTYPE := UNOFFICIAL
-    LINEAGE_EXTRAVERSION :=
-endif
-
-ifeq ($(LINEAGE_BUILDTYPE), UNOFFICIAL)
-    ifneq ($(TARGET_UNOFFICIAL_BUILD_ID),)
-        LINEAGE_EXTRAVERSION := -$(TARGET_UNOFFICIAL_BUILD_ID)
-    endif
-endif
-
-LINEAGE_VERSION_SUFFIX := $(LINEAGE_BUILD_DATE)-$(LINEAGE_BUILDTYPE)$(LINEAGE_EXTRAVERSION)-$(LINEAGE_BUILD)
+# Increase EVO Version with each major release.
+LUNARIS_VERSION := 3.0
+LUNARIS_BUILD_TYPE ?= Alpha
 
 # Internal version
-LINEAGE_VERSION := $(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(LINEAGE_VERSION_SUFFIX)
+LINEAGE_VERSION := Lunaris-AOSP-$(shell date -u +%Y%m%d%H)-$(LINEAGE_BUILD)-$(LUNARIS_VERSION)-$(LUNARIS_BUILD_TYPE)
 
 # Display version
-LINEAGE_DISPLAY_VERSION := $(PRODUCT_VERSION_MAJOR)-$(LINEAGE_VERSION_SUFFIX)
+LINEAGE_DISPLAY_VERSION := v$(LUNARIS_VERSION)-$(shell date -u +%Y%m%d)
 
-# LineageOS version properties
+# Lunaris os version properties
 PRODUCT_SYSTEM_PROPERTIES += \
-    ro.lineage.version=$(LINEAGE_VERSION) \
-    ro.lineage.display.version=$(LINEAGE_DISPLAY_VERSION) \
-    ro.lineage.build.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
-    ro.lineage.releasetype=$(LINEAGE_BUILDTYPE)
+    ro.lunaris.build.version=$(LINEAGE_VERSION) \
+    ro.lunaris.display.version=$(LINEAGE_DISPLAY_VERSION) \
+    ro.lunaris.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
+    ro.modversion=$(LUNARIS_VERSION)
