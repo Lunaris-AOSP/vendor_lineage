659ea3fdf85f1409ac61f06696cfefefd26576b2
Ghosuto <clash.raja10@gmail.com>
2025-07-26 13:44:43 +0000

    vendor: Move gms flags in common.mk
    Change-Id: I1ff68c042bc999ee298344ecd137d09235d5e413

--- config/common_full_foldable_book_telephony.mk
+++ config/common_full_foldable_book_telephony.mk
@@ -11,23 +11,3 @@ $(call inherit-product, vendor/lineage/config/tablet.mk)
 $(call inherit-product, vendor/lineage/config/telephony.mk)
 
 PRODUCT_PACKAGE_OVERLAYS += vendor/lineage/overlay/foldable_book
-
-# GMS
-WITH_GMS ?= true
-ifeq ($(WITH_GMS),true)
-  ifeq ($(TARGET_USES_OMNI_GAPPS),true)
-    $(call inherit-product, vendor/gms/gms_mini.mk)
-    $(call inherit-product, vendor/pixel-style/config/common.mk)
-    LUNARIS_PACKAGE_TYPE := Omni
-  else ifeq ($(TARGET_USES_CORE_GAPPS),true)
-    $(call inherit-product, vendor/gms/gms_pico.mk)
-    $(call inherit-product, vendor/pixel-style/config/common.mk)
-    LUNARIS_PACKAGE_TYPE := Core
-  else
-    $(call inherit-product, vendor/gms/gms_full.mk)
-    $(call inherit-product, vendor/pixel-style/config/common.mk)
-    LUNARIS_PACKAGE_TYPE := Gapps
-  endif
-else
-  LUNARIS_PACKAGE_TYPE := Vanilla
-endif
